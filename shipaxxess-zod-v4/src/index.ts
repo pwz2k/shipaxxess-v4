1ec0d28b28aba89c7829180413387a8f5c1bf1a5
Sakib Hasan <shakibhasan.me09@gmail.com>
2023-12-21 04:23:30 +0600

    update

--- shipaxxess-zod-v4/src/index.ts
+++ shipaxxess-zod-v4/src/index.ts
@@ -3,6 +3,7 @@ import * as Chats from "./chats";
 import * as Costs from "./costs";
 import * as Forget from "./forget";
 import * as Id from "./id";
+import * as Usps from "./labels";
 import * as Package from "./package";
 import * as Payment from "./payment";
 import * as Settings from "./settings";
@@ -10,7 +11,6 @@ import * as Signin from "./signin";
 import * as Signup from "./signup";
 import * as Tickets from "./tickets";
 import * as Type from "./type";
-import * as Usps from "./usps";
 import * as Weights from "./weights";
 
 export { Address, Chats, Costs, Forget, Id, Package, Payment, Settings, Signin, Signup, Tickets, Type, Usps, Weights };
