a1b51e190ed07eddce64e32f172baabff0f15e9f
Sakib Hasan <shakibhasan.me09@gmail.com>
2023-12-21 23:46:51 +0600

    update

--- shipaxxess-backend-v4/src/api/queue/labels.ts
+++ shipaxxess-backend-v4/src/api/queue/labels.ts
@@ -3,12 +3,13 @@ import { batchs } from "@schemas/batchs";
 import { eq } from "drizzle-orm";
 
 export const batchLabelQueue = async (batch: MessageBatch<{ batch_uuid: string }>, env: Bindings) => {
+	const model = new Model(env.DB);
+
 	for (const item of batch.messages) {
-		const model = new Model(env.DB);
 		const batch = await model.get(batchs, eq(batchs.uuid, item.body.batch_uuid));
 
 		for (const recipient of batch.recipients) {
-			console.log(recipient, recipient.state);
+			console.log(recipient, recipient.state, "------------WOOW");
 		}
 	}
 };
