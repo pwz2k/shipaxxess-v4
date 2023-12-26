fab0f8eec919e9c7fa7f2280751b34515570805e
Sakib Hasan <shakibhasan.me09@gmail.com>
2023-12-27 04:23:33 +0600

    update

--- shipaxxess-frontend-v4/src/modules/batchs_user/routes.tsx
+++ shipaxxess-frontend-v4/src/modules/batchs_user/routes.tsx
@@ -1,6 +1,10 @@
 import { RouteObject } from "react-router-dom";
 import BatchsUserPage from "./pages";
+import NewBatchUserPage from "./pages/new";
 
-const Routes: RouteObject[] = [{ path: "batchs", element: <BatchsUserPage /> }];
+const Routes: RouteObject[] = [
+	{ path: "batchs", element: <BatchsUserPage /> },
+	{ path: "batchs/new", element: <NewBatchUserPage /> },
+];
 
 export default Routes;
