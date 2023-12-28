7d7b900137a906176cc1248d0bcc0beb2ac4d7dc
Sakib Hasan <shakibhasan.me09@gmail.com>
2023-12-28 06:56:48 +0600

    update

--- shipaxxess-frontend-v4/src/main.tsx
+++ shipaxxess-frontend-v4/src/main.tsx
@@ -1,12 +1,10 @@
 import React from "react";
 import "@client/styles/index.css";
 import { router } from "./routes";
-import { app } from "./config/app";
 import ReactDOM from "react-dom/client";
 import { Toaster, toast } from "sonner";
 import { RouterProvider } from "react-router-dom";
 import { TimezoneProvider } from "@client/contexts/timezone";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { QueryCache, QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const queryClient = new QueryClient({
@@ -24,7 +22,6 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
 				<RouterProvider router={router} />
 			</TimezoneProvider>
 			<Toaster richColors />
-			{app.mode === "dev" && <ReactQueryDevtools initialIsOpen={false} position="left" />}
 		</QueryClientProvider>
 	</React.StrictMode>,
 );
