ba2fe17ef593c1fe20b2bc59be5a27f0036bf87a
Sakib Hasan <shakibhasan.me09@gmail.com>
2024-01-04 18:40:49 +0600

    update

--- shipaxxess-frontend-v4/src/modules/stores_user/pages/ebay.tsx
+++ shipaxxess-frontend-v4/src/modules/stores_user/pages/ebay.tsx
@@ -1,45 +1,39 @@
-// import useQuery from "@client/hooks/useQuery";
-// import { api } from "@client/lib/api";
+import { api } from "@client/lib/api";
 import React from "react";
 import { useLocation } from "react-router-dom";
 
 const StoreEbayUserPage = () => {
 	const { search } = useLocation();
 
-	// const exchangeCode = async (code: string) => {
-	// 	console.log(code);
+	const exchangeCode = async (code: string) => {
+		console.log(code);
 
-	// 	const req = await api.url(`/user/stores/ebay/callback?code=${code}`).useAuth().get();
-	// 	const res = await req.json<{ success: boolean; id: number }>();
+		const req = await api.url(`/user/stores/ebay/callback?code=${code}`).useAuth().get();
+		const res = await req.json<{ success: boolean; id: number }>();
 
-	// 	if (!res.success) {
-	// 		api.showErrorToast();
-	// 		return;
-	// 	}
+		if (!res.success) {
+			api.showErrorToast();
+			return;
+		}
 
-	// 	api.showSuccessToast("Successfully connected to eBay!");
+		api.showSuccessToast("Successfully connected to eBay!");
 
-	// 	const req1 = await api.url(`/user/stores/ebay/fetch?id=${res.id}`).useAuth().get();
-	// 	const res1 = await req1.json<{ success: boolean }>();
+		const req1 = await api.url(`/user/stores/ebay/fetch?id=${res.id}`).useAuth().get();
+		const res1 = await req1.json<{ success: boolean }>();
 
-	// 	if (!res1.success) {
-	// 		api.showErrorToast();
-	// 		return;
-	// 	}
+		if (!res1.success) {
+			api.showErrorToast();
+			return;
+		}
 
-	// 	console.log(res1);
-	// };
+		console.log(res1);
+	};
 
 	React.useEffect(() => {
-		console.log(search);
+		const code = search.match(/\?code=([^&]+)/);
+		if (!code) return;
 
-		const url = new URLSearchParams(search);
-
-		console.log(url.get("code")!);
-
-		// if (query.get("code")) {
-		// 	exchangeCode(query.get("code")!);
-		// }
+		exchangeCode(code[1]);
 	}, [search]);
 
 	return <div>StoreEbayUserPage</div>;
