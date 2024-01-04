3dcbf0a0450dad5d69936107cb5ec09702274374
Sakib Hasan <shakibhasan.me09@gmail.com>
2024-01-04 19:05:45 +0600

    update

--- shipaxxess-frontend-v4/src/modules/stores_user/pages/ebay.tsx
+++ shipaxxess-frontend-v4/src/modules/stores_user/pages/ebay.tsx
@@ -1,3 +1,4 @@
+import useQuery from "@client/hooks/useQuery";
 import { api } from "@client/lib/api";
 import React from "react";
 import { useLocation, useNavigate } from "react-router-dom";
@@ -5,6 +6,7 @@ import { useLocation, useNavigate } from "react-router-dom";
 const StoreEbayUserPage = () => {
 	const { search } = useLocation();
 
+	const query = useQuery();
 	const navigate = useNavigate();
 
 	const exchangeCode = async (code: string) => {
@@ -21,11 +23,29 @@ const StoreEbayUserPage = () => {
 		navigate(`/stores/ebay?import_id=${res.id}`);
 	};
 
+	const getOders = async (id: string) => {
+		const req = await api.url(`/user/stores/ebay/fetch?id=${id}`).useAuth().get();
+		const res = await req.json<{ success: boolean }>();
+
+		if (!res.success) {
+			api.showErrorToast();
+			return;
+		}
+
+		console.log(res);
+
+		api.showSuccessToast("Successfully imported orders!");
+	};
+
 	React.useEffect(() => {
 		const code = search.match(/\?code=([^&]+)/);
-		if (!code) return;
+		if (code) {
+			exchangeCode(code[1]);
+		}
 
-		exchangeCode(code[1]);
+		if (query.get("import_id")) {
+			getOders(query.get("import_id")!);
+		}
 	});
 
 	return <div>StoreEbayUserPage</div>;
