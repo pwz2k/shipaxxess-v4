e2520dbdcedb76496f1d7fad0f0db0e48d2b8510
Sakib Hasan <shakibhasan.me09@gmail.com>
2024-01-05 06:00:44 +0600

    update

--- shipaxxess-backend-v4/src/api/user/stores.ts
+++ shipaxxess-backend-v4/src/api/user/stores.ts
@@ -1,7 +1,14 @@
+import { Model } from "@lib/model";
+import { stores } from "@schemas/stores";
+import { eq } from "drizzle-orm";
 import { Context } from "hono";
 
 const GetAll = async (c: Context<App>) => {
-	return c.json([]);
+	const model = new Model(c.env.DB);
+
+	const st = await model.all(stores, eq(stores.user_id, c.get("jwtPayload").id));
+
+	return c.json(st);
 };
 
 export const StoreUser = { GetAll };
