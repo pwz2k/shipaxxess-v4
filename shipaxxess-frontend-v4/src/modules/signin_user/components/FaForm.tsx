73a152b7937dc1a5f9b318dbb5347bfc500343d6
Sakib Hasan <shakibhasan.me09@gmail.com>
2024-01-09 16:06:15 +0600

    update

--- shipaxxess-frontend-v4/src/modules/signin_user/components/FaForm.tsx
+++ shipaxxess-frontend-v4/src/modules/signin_user/components/FaForm.tsx
@@ -5,21 +5,28 @@ import { api } from "@client/lib/api";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Verify } from "@shipaxxess/shipaxxess-zod-v4";
 import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
 
 const FaForm = ({ code }: { code: string | null }) => {
-	const form = useForm<Verify.ZODSCHEMA>({ defaultValues: { code: "" }, resolver: zodResolver(Verify.ZODSCHEMA) });
+	const form = useForm<Verify.ZODSCHEMA>({
+		defaultValues: { code: "", email_address: code!, type: "two_fa" },
+		resolver: zodResolver(Verify.ZODSCHEMA),
+	});
+
+	const navigate = useNavigate();
 
 	const { button, setIsLoading } = useLoading({ label: "Submit", className: "w-full" });
 
 	const submit = async (data: Verify.ZODSCHEMA) => {
 		setIsLoading(true);
 
-		const req = await api.url("/two_fa").post({ code: data.code, email_address: code, type: "two_fa" });
+		const req = await api.url("/two_fa").post(data);
 		const res = await req.json<{ token: string }>();
 
 		if (res.token) {
 			localStorage.setItem("token", res.token);
 			api.showSuccessToast();
+			navigate("/dashboard");
 			return;
 		}
 
@@ -36,6 +43,20 @@ const FaForm = ({ code }: { code: string | null }) => {
 					render={({ field }) => (
 						<FormItem>
 							<FormLabel>Verification Code</FormLabel>
+							<FormControl>
+								<Input {...field} autoComplete="on" placeholder="Enter the six digit verification code here" />
+							</FormControl>
+							<FormMessage />
+						</FormItem>
+					)}
+				/>
+
+				<FormField
+					control={form.control}
+					name="code"
+					render={({ field }) => (
+						<FormItem className="hidden">
+							<FormLabel>Verification Email</FormLabel>
 							<FormControl>
 								<Input {...field} autoComplete="on" placeholder="Enter the six digit verification code here" />
 							</FormControl>
