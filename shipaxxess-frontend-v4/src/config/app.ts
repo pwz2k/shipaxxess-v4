848f46bee65c886a3faa02875adb298ca8855fc5
Sakib Hasan <shakibhasan.me09@gmail.com>
2024-01-24 01:45:11 +0600

    update

--- shipaxxess-frontend-v4/src/config/app.ts
+++ shipaxxess-frontend-v4/src/config/app.ts
@@ -1,12 +1,12 @@
 export const app = {
-	name: "Demo",
+	name: "xclusivelabels",
 	host: "http://localhost:5173",
 	prod_host: "https://shipaxxess-v4.pages.dev",
 	api: "http://localhost:8787/v1",
 	prod_api: "https://shipaxxess-backend-v4.pwz2k.workers.dev/v1",
 	mode: import.meta.env.VITE_MODE,
 	version: "v4.0-dev",
-	support: "support@gmail.com",
+	support: "support@xclusivelabels.com",
 	time: {
 		format: "dddd, M/D/YY hh:mm A",
 	},
