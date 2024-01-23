5f55752699545b9b5796fa9914d0f6659987f577
Sakib Hasan <shakibhasan.me09@gmail.com>
2024-01-24 01:44:28 +0600

    update

--- shipaxxess-frontend-v4/vite.config.ts
+++ shipaxxess-frontend-v4/vite.config.ts
@@ -1,9 +1,8 @@
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
-import Pages from "vite-plugin-pages";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react(), tsconfigPaths(), Pages()],
+	plugins: [react(), tsconfigPaths()],
 });
