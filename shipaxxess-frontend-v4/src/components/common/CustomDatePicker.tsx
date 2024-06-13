67dbc3731d13ec63a1688329855de3fb83d55f7b
aamar-shahzad <raiaamirshahzad72@gmail.com>
2024-06-13 15:33:45 -0400

    Refactor DashboardStats component to use loading state and add date range picker functionality

--- shipaxxess-frontend-v4/src/components/common/CustomDatePicker.tsx
+++ shipaxxess-frontend-v4/src/components/common/CustomDatePicker.tsx
@@ -3,6 +3,7 @@ import { DateRangePicker } from 'react-date-range';
 import { addDays, startOfMonth, endOfMonth, subDays } from 'date-fns';
 import 'react-date-range/dist/styles.css'; // main css file
 import 'react-date-range/dist/theme/default.css'; // theme css file
+import { is } from 'date-fns/locale';
 
 interface CustomDateRangePickerProps {
     onDateChange: (start: Date | null, end: Date | null) => void;
@@ -14,8 +15,10 @@ const CustomDateRangePicker: React.FC<CustomDateRangePickerProps> = ({ onDateCha
             startDate: new Date(),
             endDate: new Date(),
             key: 'selection'
+
         }
     ]);
+    const [isOpen, setIsOpen] = useState(false);
 
     const predefinedRanges = [
         {
@@ -53,26 +56,93 @@ const CustomDateRangePicker: React.FC<CustomDateRangePickerProps> = ({ onDateCha
                 endDate: endOfMonth(new Date()),
             }),
         },
+        {
+            label: 'Last Month',
+            range: () => ({
+                startDate: startOfMonth(subDays(new Date(), 30)),
+                endDate: endOfMonth(subDays(new Date(), 30)),
+            }),
+        },
+        {
+            label: 'Last 3 Months',
+            range: () => ({
+                startDate: subDays(new Date(), 90),
+                endDate: new Date(),
+            }),
+        },
+        {
+            label: 'Last 6 Months',
+            range: () => ({
+                startDate: subDays(new Date(), 180),
+                endDate: new Date(),
+            }),
+        },
+        {
+            label: 'Last 1 Year',
+            range: () => ({
+                startDate: subDays(new Date(), 365),
+                endDate: new Date(),
+            }),
+        },
+        // {
+        //     label: 'Custom Range',
+        //     range: () => ({
+        //         startDate: new Date(),
+        //         endDate: new Date(),
+        //     }),
+
+
+        // }
+
     ];
 
     const handleSelect = (ranges: any) => {
         const { startDate, endDate } = ranges.selection;
+        setIsOpen(false);
         setState([ranges.selection]);
         onDateChange(startDate, endDate);
+
+    };
+    const handleSelectChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+        const range = predefinedRanges.find(range => range.label === event.target.value);
+
+        if (range) {
+            const { startDate, endDate } = range.range();
+            setState([{ startDate, endDate, key: 'selection' }]);
+            onDateChange(startDate, endDate);
+        }
+        if (range?.label === 'Custom Range') setIsOpen(true);
     };
 
     return (
         <div>
-            <DateRangePicker
+            <label htmlFor="date-range" className='px-1 mx-1'>Choose Date:</label>
+            <select onChange={handleSelectChange}>
+                {predefinedRanges.map(({ label }) => (
+                    <option key={label} value={label}>
+                        {label}
+                    </option>
+                ))}
+            </select>
+
+
+            {/* {isOpen && <DateRangePicker
                 ranges={state}
+                showMonthArrow={true}
+
                 onChange={handleSelect}
-                staticRanges={predefinedRanges.map(({ label, range }) => ({
-                    label,
-                    range,
-                    isSelected: () => false,
-                }))}
+                // staticRanges={predefinedRanges.map(({ label, range }) => ({
+                //     label,
+                //     range,
+                //     isSelected: () => false,
+                // }))}
                 inputRanges={[]}
-            />
+            />}
+ */}
+
+
+
+
         </div>
     );
 };
--- shipaxxess-frontend-v4/src/modules/dashboard_user/pages/index.tsx
+++ shipaxxess-frontend-v4/src/modules/dashboard_user/pages/index.tsx
@@ -1,87 +1,4 @@
-// import React, { useState, useEffect } from 'react';
-// import TopStatesBarChart from './components/TopStatesBarChart';
-// import PendingRefundsCard from './components/PendingRefundsCard';
-// import AverageCostCard from './components/AverageCostCard';
-// import TotalShipmentsLineChart from './components/TotalShipmentsLineChart';
-// import OverviewSection from './components/OverviewSection';
-// import Title from '@client/components/common/title';
-// import { CreditCard, LayoutDashboardIcon, Share2 } from 'lucide-react';
-// import DateRangePicker from '../../../components/common/DateRangePicker';
 
-// const DashboardStats: React.FC = () => {
-// 	const getRandomNumber = (min: number, max: number) => {
-// 		return Math.floor(Math.random() * (max - min + 1) + min);
-// 	};
-
-// 	// State for loading
-// 	const [isLoading, setIsLoading] = useState<boolean>(true);
-
-// 	// Simulating data loading with setTimeout
-// 	useEffect(() => {
-// 		const timeout = setTimeout(() => {
-// 			setIsLoading(false);
-// 		}, 2000);
-
-// 		return () => clearTimeout(timeout);
-// 	}, []);
-
-// 	// State for date range
-// 	const [startDate, setStartDate] = useState<Date | null>(null);
-// 	const [endDate, setEndDate] = useState<Date | null>(null);
-
-// 	// Handle date change
-// 	const handleDateChange = (start: Date | null, end: Date | null) => {
-// 		setStartDate(start);
-// 		setEndDate(end);
-// 	};
-
-// 	return (
-// 		<>
-// 			{/* Overlay and backdrop when loading */}
-// 			{isLoading && (
-// 				<div className="fixed top-0 left-0 z-50 w-full h-full flex items-center justify-center bg-black bg-opacity-50">
-// 					<div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-gray-900"></div>
-// 				</div>
-// 			)}
-
-// 			<div className="px-4">
-// 				<div className='flex justify-between'>
-// 					<div className='flex items-center gap-x-1 sticky top-0 '>
-// 						<LayoutDashboardIcon size={24} />
-// 						<Title title="Dashboard" />
-// 					</div>
-// 					<DateRangePicker
-// 						_startDate={startDate}
-// 						_endDate={endDate}
-// 						onDateChange={handleDateChange}
-// 					/>
-// 				</div>
-// 				<div className="grid grid-cols-1 md:grid-cols-4 gap-4 mb-4 flex-1">
-// 					<OverviewSection title="Current Balance" value={getRandomNumber(1000, 10000)} icon={<CreditCard />} />
-// 					<OverviewSection title="Total Spend" value={getRandomNumber(500, 5000)} />
-// 					<OverviewSection title="Total Refund" value={getRandomNumber(100, 1000)} />
-// 					<OverviewSection title="Referral Balance" value={getRandomNumber(500, 5000)} icon={<Share2 />} />
-// 				</div>
-// 			</div>
-// 			<div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
-// 				<div className="col-span-1 lg:col-span-2">
-// 					<TopStatesBarChart />
-// 				</div>
-// 				<div className="col-span-1">
-// 					<PendingRefundsCard />
-// 				</div>
-// 				<div className="col-span-1">
-// 					<AverageCostCard />
-// 				</div>
-// 				<div className="col-span-1 lg:col-span-2">
-// 					<TotalShipmentsLineChart />
-// 				</div>
-// 			</div>
-// 		</>
-// 	);
-// };
-
-// export default DashboardStats;
 import React, { useState, useEffect } from 'react';
 import TopStatesBarChart from './components/TopStatesBarChart';
 import PendingRefundsCard from './components/PendingRefundsCard';
@@ -134,6 +51,7 @@ const DashboardStats: React.FC = () => {
 						<LayoutDashboardIcon size={24} />
 						<Title title="Dashboard" />
 					</div>
+
 					<CustomDateRangePicker onDateChange={handleDateChange} />
 				</div>
 				<div className="grid grid-cols-1 md:grid-cols-4 gap-4 mb-4 flex-1">
